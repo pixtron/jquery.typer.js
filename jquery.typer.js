0af9cf55613c2421de4559c3a7e2f49ae2ab919c
Stefan Aebischer <typo3@pixtron.ch>
2014-09-30 01:11:05 +0100

    Marks text before deleting it

--- jquery.typer.js
+++ jquery.typer.js
@@ -9,11 +9,13 @@
 (function($){
     $.fn.typer = function(text, options){
         options = $.extend({}, {
-            char: '',
             delay: 2000,
+            markDelay: 300,
+            markDuration: 200,
             duration: 600,
             endless: true,
             onType: $.noop,
+            onMark: $.noop,
             afterAll: $.noop,
             afterPhrase: $.noop
         }, options || text);
@@ -23,7 +25,6 @@
 
         return this.each(function(){
             var elem = $(this),
-                isVal = {input:1, textarea:1}[this.tagName.toLowerCase()],
                 isTag = false,
                 timer,
                 c = 0;
@@ -34,7 +35,8 @@
 
                 if( char === '<' ){ isTag = true; }
                 if( char === '>' ){ isTag = false; }
-                elem[isVal ? "val" : "html"](e.substr(0, c) + ($.isFunction(options.char) ? options.char() : options.char || ' '));
+                elem.html(e.substr(0, c));
+
                 if(c <= e.length){
                     if( isTag ){
                         typetext(i);
@@ -43,17 +45,41 @@
                     }
                     options.onType(timer);
                 } else {
-                    c = 0;
-                    i++;
+                    timer = setTimeout(marktext, options.delay, i);
+                }
 
-                    if (i === text.length && !options.endless) {
-                        return;
-                    } else if (i === text.length) {
-                        i = 0;
+                function marktext(i) {
+                    var e = ({string:1, number:1}[typeof text] ? text : text[i]) + '',
+                        char = e.substr(c--, 1);
+
+                    if( char === '>' ){ isTag = true; }
+                    if( char === '<' ){ isTag = false; }
+                    var markedText = $('<span class="highlighted" />').html(e.substr(c, e.length));
+
+                    elem.html(e.substr(0, c)).append(markedText);
+
+                    if(c > 0){
+                        if( isTag ){
+                            marktext(i);
+                        } else {
+                            timer = setTimeout(marktext, options.markDuration/10, i);
+                        }
+                        options.onMark(timer);
+                    } else {
+                        c = 0;
+                        i++;
+
+                        if (i === text.length && !options.endless) {
+                            return;
+                        } else if (i === text.length) {
+                            i = 0;
+                        }
+
+                        timer = setTimeout(typetext, options.markDelay, i);
+                        if(i === text.length - 1) options.afterAll(timer);
+                        options.afterPhrase(timer);
+
                     }
-                    timer = setTimeout(typetext, options.delay, i);
-                    if(i === text.length - 1) options.afterAll(timer);
-                    options.afterPhrase(timer);
                 }
             })(0);
         });
